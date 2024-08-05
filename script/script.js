0ee00f1b356ee60b83bec57f559d386d569017cb
mansi-keer <c0941062@mylambton.ca>
2024-08-05 18:49:27 -0400

    work week till 13

--- script/script.js
+++ script/script.js
@@ -3,26 +3,6 @@ File Name: script.js
 Date:3aug
 */
 
-//Hamburger menu function
-/*function hamburger() {
-
-    var menu = document.getElementById("menu-links");
-    var logo = document.getElementById("ffc-logo");
-
-if (menu.style.display === "block" && logo.style.display === "none") 
-    { 
-    menu.style.display ="none";
-    logo.style.display = "block";
-}
-else {
-    menu.style.display ="block";
-    logo.style.display = "none";
-}
-}*/
-
-
-
-// media.js
 
 // Lightbox functionality for image gallery
 document.addEventListener('DOMContentLoaded', function() {
